4b8922a4d52322b2927e03d6e7e6cfd96b3be515
stasmaj <s.majevski@cointraffic.io>
2019-03-13 23:16:10 +0200

    Added: GET response from weather API

--- app/src/main/java/com/mobile/development/weatherapp/MainActivity.java
+++ app/src/main/java/com/mobile/development/weatherapp/MainActivity.java
@@ -1,10 +1,24 @@
 package com.mobile.development.weatherapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +26,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String apiKey = BuildConfig.ApiKey;
+
+        // Tallinn coordinates
+        double latitude = 59.436962;
+        double logitude = 24.753574;
+
+        String forecastUrl ="https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + logitude;
+
+        if(isNetworkAvaliable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+            Call call = client.newCall(request);
+
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    try {
+                        String jsonData = response.body() != null ? response.body().string() : null;
+                        Log.v(TAG, jsonData);
+
+                        if(response.isSuccessful()) {
+                            //TODO Parse json on success response
+                        } else {
+                            //TODO notify user about response error
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught",e);
+                    }
+                }
+            });
+        }
+    }
+
+    private boolean isNetworkAvaliable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        boolean isAvaliable = false;
+
+        if (manager != null) {
+            networkInfo = manager.getActiveNetworkInfo();
+        }
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvaliable = true;
+        }
+        return isAvaliable;
     }
 }
