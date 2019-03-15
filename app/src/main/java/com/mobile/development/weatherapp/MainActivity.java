3d3d7555f1ac02bd0f7df156fd43af30b436dbf6
Kristjan Liiva <kristjanliiva@gmail.com>
2019-03-16 00:04:40 +0200

    #3: Added data displaying to MainActivity

--- app/src/main/java/com/mobile/development/weatherapp/MainActivity.java
+++ app/src/main/java/com/mobile/development/weatherapp/MainActivity.java
@@ -3,88 +3,114 @@ package com.mobile.development.weatherapp;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.mobile.development.weatherapp.model.Weather;
+import com.mobile.development.weatherapp.service.WeatherServiceImpl;
 
 import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.text.MessageFormat;
+import java.time.Instant;
+import java.time.ZoneId;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Weather currentWeather;
+    private WeatherServiceImpl weatherService;
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    // UI elements
+    private TextView mTemperatureLabel;
+    private TextView mTimeLabel;
+    private TextView mLocationLabel;
+    private ImageView mIconImage;
+    private TextView mHumidityValue;
+    private TextView mPrecipValue;
+    private TextView mSummaryLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String apiKey = BuildConfig.ApiKey;
+        weatherService = new WeatherServiceImpl();
+        mTemperatureLabel = findViewById( R.id.temperatureLabel );
+        mTimeLabel = findViewById( R.id.timeLabel );
+        mLocationLabel = findViewById( R.id.locationLabel );
+        mIconImage = findViewById( R.id.iconImageView );
+        mHumidityValue = findViewById( R.id.humidityValue );
+        mPrecipValue = findViewById( R.id.precipValue );
+        mSummaryLabel = findViewById( R.id.summaryLabel );
 
-        // Tallinn coordinates
-        double latitude = 59.436962;
-        double logitude = 24.753574;
+        new AsyncClassTask().execute();
+    }
 
-        String forecastUrl ="https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + logitude;
-        currentWeather = new Weather();
+    private class AsyncClassTask extends AsyncTask< Void, Void, Weather >
+    {
 
-        if(isNetworkAvaliable()) {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(forecastUrl)
-                    .build();
-            Call call = client.newCall(request);
+        private Weather retrievedWeather;
 
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+        @Override
+        protected Weather doInBackground(Void... voids) {
+            // Tallinn coordinates
+            double latitude = 59.436962;
+            double longitude = 24.753574;
+
+            if( isNetworkAvailable() )
+            {
+                try
+                {
+                    retrievedWeather = weatherService.retrieveCurrentWeather( latitude, longitude );
                 }
-
-                @Override
-                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                    try {
-                        String json = response.body() != null ? response.body().string() : null;
-                        Log.v(TAG, json);
-
-                        if(response.isSuccessful()) {
-                            JSONObject forecast = new JSONObject(json);
-                            JSONObject currently = forecast.getJSONObject("currently");
-
-                            //TODO Parse json on success response
-                        } else {
-                            //TODO notify user about response error
-                        }
-                    } catch (IOException e) {
-                        Log.e(TAG, "Exception caught",e);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                catch ( IOException e )
+                {
+                    Log.v( TAG, MessageFormat.format( "Failed to retrieve weather for the provided location: {0}", e.getMessage() ) );
+                    Toast.makeText( getApplicationContext(), "Could not retrieve weather, please restart the app in order to retry", Toast.LENGTH_SHORT ).show();
                 }
-            });
+            }
+            else
+            {
+                Toast.makeText( getApplicationContext(), "Please enable a network connection", Toast.LENGTH_SHORT ).show();
+            }
+
+            return null;
+        }
+
+        private void initializeUI() {
+            mTemperatureLabel.setText( Long.toString( Math.round( retrievedWeather.getTemperature() ) ) );
+            mTimeLabel.setText( Instant.ofEpochMilli( retrievedWeather.getTime() ).atZone( ZoneId.systemDefault() ).toLocalDate().toString() );
+            mSummaryLabel.setText( retrievedWeather.getSummary() );
+            mPrecipValue.setText( Double.toString( retrievedWeather.getPrecipChance() ) );
+            mHumidityValue.setText( Double.toString( retrievedWeather.getHumidity() ) );
+            mIconImage.setImageResource( getResources().getIdentifier( retrievedWeather.getIcon(), "drawable", getPackageName() ) );
+        }
+
+        @Override
+        protected void onPostExecute( Weather result )
+        {
+            initializeUI();
+            super.onPostExecute( result );
         }
     }
 
-    private boolean isNetworkAvaliable() {
+    private boolean isNetworkAvailable() {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = null;
-        boolean isAvaliable = false;
+        boolean isAvailable = false;
 
-        if (manager != null) {
+        if ( manager != null )
+        {
             networkInfo = manager.getActiveNetworkInfo();
         }
-        if(networkInfo != null && networkInfo.isConnected()){
-            isAvaliable = true;
+        if( networkInfo != null && networkInfo.isConnected() )
+        {
+            isAvailable = true;
         }
-        return isAvaliable;
+        return isAvailable;
     }
 }
